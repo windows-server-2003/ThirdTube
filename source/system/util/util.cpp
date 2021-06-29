efe001f5689c34168733e7477410847e67adaec2
QCFium <rin20042004@gmail.com>
2021-06-29 17:34:00 +0900

    Speeded up time format converter

--- source/system/util/util.cpp
+++ source/system/util/util.cpp
@@ -33,31 +33,17 @@ Result_with_string Util_parse_file(std::string source_data, int num_of_items, st
 
 std::string Util_convert_seconds_to_time(double input_seconds)
 {
+	if (input_seconds == INFINITY) return "INF"; // for debug purpose
 	int hours = 0;
 	int minutes = 0;
 	int seconds = 0;
 	long count = 0;
 	std::string time = "";
-
-	for(count = 0; count < (int)input_seconds; count++)
-	{
-		if(seconds + 1 >= 60)
-		{
-			if(minutes + 1 >= 60)
-			{
-				seconds = 0;
-				minutes = 0;
-				hours++;
-			}
-			else
-			{
-				seconds = 0;
-				minutes++;
-			}
-		}
-		else
-			seconds++;
-	}
+	
+	count = (long) input_seconds;
+	seconds = count % 60;
+	minutes = count / 60 % 60;
+	hours = count / 3600;
 
 	if(hours != 0)
 		time += std::to_string(hours) + ":";
@@ -75,6 +61,7 @@ std::string Util_convert_seconds_to_time(double input_seconds)
 	time += std::to_string(input_seconds - count + 1).substr(1, 2);
 	return time;
 }
+
 
 std::string Util_encode_to_escape(std::string in_data)
 {
