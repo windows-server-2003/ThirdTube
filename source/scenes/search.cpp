178b00318768833be6d67a487c7a26fb9957dc55
QCFium <rin20042004@gmail.com>
2021-07-14 18:48:25 +0900

    Improved responsivity of search result scene

--- source/scenes/search.cpp
+++ source/scenes/search.cpp
@@ -23,6 +23,7 @@ namespace Search {
 	bool search_request = false;
 	bool load_more_request = false; // 
 	YouTubeSearchResult search_result;
+	std::vector<std::vector<std::string> > wrapped_titles;
 	Thread search_thread;
 	
 	int scroll_offset = 0;
@@ -66,107 +67,6 @@ static void send_load_more_request() {
 	svcReleaseMutex(resource_lock);
 }
 
-static void search_thread_func(void* arg) {
-	(void) arg;
-	
-	while (!exiting) {
-		if (search_request) {
-			std::string search_url = "https://m.youtube.com/results?search_query=";
-			for (auto c : cur_search_word) {
-				search_url.push_back('%');
-				search_url.push_back("0123456789ABCDEF"[(u8) c / 16]);
-				search_url.push_back("0123456789ABCDEF"[(u8) c % 16]);
-			}
-			add_cpu_limit(25);
-			YouTubeSearchResult new_result = youtube_parse_search(search_url);
-			remove_cpu_limit(25);
-			
-			svcWaitSynchronization(resource_lock, std::numeric_limits<s64>::max());
-			search_result = new_result;
-			for (auto i : search_result.results)
-				if (i.type == YouTubeSearchResult::Item::VIDEO) request_thumbnail(i.video.thumbnail_url);
-			svcReleaseMutex(resource_lock);
-			
-			scroll_offset = 0;
-			reset_hid_state();
-			
-			search_request = false;
-		} else if (load_more_request) {
-			auto new_result = youtube_continue_search(search_result);
-			
-			// if another search was made and load_more_request was thus disabled, we should not modify the cleared result
-			if (load_more_request) {
-				svcWaitSynchronization(resource_lock, std::numeric_limits<s64>::max());
-				if (new_result.error != "") search_result.error = new_result.error;
-				else {
-					for (size_t i = search_result.results.size(); i < new_result.results.size(); i++)
-						if (new_result.results[i].type == YouTubeSearchResult::Item::VIDEO) request_thumbnail(new_result.results[i].video.thumbnail_url);
-					search_result = new_result;
-				}
-				svcReleaseMutex(resource_lock);
-			}
-			load_more_request = false;
-		} else usleep(DEF_ACTIVE_THREAD_SLEEP_TIME);
-		
-		while (thread_suspend)
-			usleep(DEF_INACTIVE_THREAD_SLEEP_TIME);
-	}
-	
-	threadExit(0);
-}
-
-bool Search_query_init_flag(void) {
-	return already_init;
-}
-
-
-void Search_resume(std::string arg)
-{
-	(void) arg;
-	reset_hid_state();
-	thread_suspend = false;
-	var_need_reflesh = true;
-}
-
-void Search_suspend(void)
-{
-	thread_suspend = true;
-}
-
-void Search_init(void)
-{
-	Util_log_save("search/init", "Initializing...");
-	Result_with_string result;
-	
-	svcCreateMutex(&resource_lock, false);
-	
-	reset_hid_state();
-	scroll_offset = 0;
-	search_result = YouTubeSearchResult();
-	search_request = false;
-	load_more_request = false;
-	
-	// result = Util_load_msg("sapp0_" + var_lang + ".txt", vid_msg, DEF_SEARCH_NUM_OF_MSG);
-	// Util_log_save(DEF_SAPP0_INIT_STR, "Util_load_msg()..." + result.string + result.error_description, result.code);
-
-	search_thread = threadCreate(search_thread_func, (void*)(""), DEF_STACKSIZE, DEF_THREAD_PRIORITY_NORMAL, 0, false);
-	Search_resume("");
-	already_init = true;
-}
-
-void Search_exit(void)
-{
-	already_init = false;
-	thread_suspend = false;
-	exiting = true;
-	
-	u64 time_out = 10000000000;
-	
-	Util_log_save(DEF_SAPP0_EXIT_STR, "threadJoin()...", threadJoin(search_thread, time_out));
-	threadFree(search_thread);
-
-	Util_log_save("search/exit", "Exited.");
-}
 
 // truncate and wrap into two lines if necessary
 static std::vector<std::string> truncate_str(std::string input_str, int max_width, double x_size, double y_size) {
@@ -259,7 +159,133 @@ static std::vector<std::string> truncate_str(std::string input_str, int max_widt
 	}
 }
 
-static void draw_search_result(const YouTubeSearchResult &result, Hid_info key, int color) {
+static void search_thread_func(void* arg) {
+	(void) arg;
+	
+	while (!exiting) {
+		if (search_request) {
+			std::string search_url = "https://m.youtube.com/results?search_query=";
+			for (auto c : cur_search_word) {
+				search_url.push_back('%');
+				search_url.push_back("0123456789ABCDEF"[(u8) c / 16]);
+				search_url.push_back("0123456789ABCDEF"[(u8) c % 16]);
+			}
+			add_cpu_limit(25);
+			YouTubeSearchResult new_result = youtube_parse_search(search_url);
+			remove_cpu_limit(25);
+			
+			// wrap and truncate here
+			Util_log_save("search", "truncate start");
+			std::vector<std::vector<std::string> > new_wrapped_titles(new_result.results.size());
+			for (size_t i = 0; i < new_result.results.size(); i++) {
+				std::string cur_str = new_result.results[i].type == YouTubeSearchResult::Item::VIDEO ? new_result.results[i].video.title : new_result.results[i].channel.name;
+				new_wrapped_titles[i] = truncate_str(cur_str, 320 - (THUMBNAIL_WIDTH + 3), 0.5, 0.5);
+			}
+			Util_log_save("search", "truncate end");
+			
+			
+			svcWaitSynchronization(resource_lock, std::numeric_limits<s64>::max());
+			search_result = new_result;
+			wrapped_titles = new_wrapped_titles;
+			for (auto i : search_result.results)
+				if (i.type == YouTubeSearchResult::Item::VIDEO) request_thumbnail(i.video.thumbnail_url);
+			svcReleaseMutex(resource_lock);
+			
+			scroll_offset = 0;
+			reset_hid_state();
+			
+			search_request = false;
+		} else if (load_more_request) {
+			auto new_result = youtube_continue_search(search_result);
+			
+			// if another search was made and load_more_request was thus disabled, we should not modify the cleared result
+			if (load_more_request) {
+				// wrap and truncate here
+				Util_log_save("search", "truncate start");
+				std::vector<std::vector<std::string> > new_wrapped_titles(new_result.results.size());
+				for (size_t i = 0; i < new_result.results.size(); i++) {
+					std::string cur_str = new_result.results[i].type == YouTubeSearchResult::Item::VIDEO ? new_result.results[i].video.title : new_result.results[i].channel.name;
+					new_wrapped_titles[i] = truncate_str(cur_str, 320 - (THUMBNAIL_WIDTH + 3), 0.5, 0.5);
+				}
+				Util_log_save("search", "truncate end");
+				
+				svcWaitSynchronization(resource_lock, std::numeric_limits<s64>::max());
+				if (load_more_request) {
+					if (new_result.error != "") search_result.error = new_result.error;
+					else {
+						for (size_t i = search_result.results.size(); i < new_result.results.size(); i++)
+							if (new_result.results[i].type == YouTubeSearchResult::Item::VIDEO) request_thumbnail(new_result.results[i].video.thumbnail_url);
+						search_result = new_result;
+						wrapped_titles = new_wrapped_titles;
+					}
+				}
+				svcReleaseMutex(resource_lock);
+			}
+			load_more_request = false;
+		} else usleep(DEF_ACTIVE_THREAD_SLEEP_TIME);
+		
+		while (thread_suspend)
+			usleep(DEF_INACTIVE_THREAD_SLEEP_TIME);
+	}
+	
+	threadExit(0);
+}
+
+bool Search_query_init_flag(void) {
+	return already_init;
+}
+
+
+void Search_resume(std::string arg)
+{
+	(void) arg;
+	reset_hid_state();
+	thread_suspend = false;
+	var_need_reflesh = true;
+}
+
+void Search_suspend(void)
+{
+	thread_suspend = true;
+}
+
+void Search_init(void)
+{
+	Util_log_save("search/init", "Initializing...");
+	Result_with_string result;
+	
+	svcCreateMutex(&resource_lock, false);
+	
+	reset_hid_state();
+	scroll_offset = 0;
+	search_result = YouTubeSearchResult();
+	search_request = false;
+	load_more_request = false;
+	
+	// result = Util_load_msg("sapp0_" + var_lang + ".txt", vid_msg, DEF_SEARCH_NUM_OF_MSG);
+	// Util_log_save(DEF_SAPP0_INIT_STR, "Util_load_msg()..." + result.string + result.error_description, result.code);
+
+	search_thread = threadCreate(search_thread_func, (void*)(""), DEF_STACKSIZE, DEF_THREAD_PRIORITY_NORMAL, 0, false);
+	Search_resume("");
+	already_init = true;
+}
+
+void Search_exit(void)
+{
+	already_init = false;
+	thread_suspend = false;
+	exiting = true;
+	
+	u64 time_out = 10000000000;
+	
+	Util_log_save(DEF_SAPP0_EXIT_STR, "threadJoin()...", threadJoin(search_thread, time_out));
+	threadFree(search_thread);
+
+	Util_log_save("search/exit", "Exited.");
+}
+
+
+static void draw_search_result(const YouTubeSearchResult &result, const std::vector<std::vector<std::string> > &wrapped_titles, Hid_info key, int color) {
 	if (result.results.size()) {
 		for (size_t i = 0; i < result.results.size(); i++) {
 			int y_l = RESULT_Y_LOW + i * FONT_VERTICAL_INTERVAL - scroll_offset;
@@ -273,7 +299,7 @@ static void draw_search_result(const YouTubeSearchResult &result, Hid_info key, 
 			if (result.results[i].type == YouTubeSearchResult::Item::VIDEO) {
 				auto cur_video = result.results[i].video;
 				// title
-				auto title_lines = truncate_str(cur_video.title, 320 - (THUMBNAIL_WIDTH + 3), 0.5, 0.5);
+				auto title_lines = wrapped_titles[i];
 				for (size_t line = 0; line < title_lines.size(); line++) {
 					Draw(title_lines[line], THUMBNAIL_WIDTH + 3, y_l + line * 13, 0.5, 0.5, color);
 				}
@@ -320,6 +346,7 @@ Intent Search_draw(void)
 	}
 	svcWaitSynchronization(resource_lock, std::numeric_limits<s64>::max());
 	auto search_result_bak = search_result;
+	auto wrapped_titles_bak = wrapped_titles;
 	svcReleaseMutex(resource_lock);
 
 	if(var_need_reflesh || !var_eco_mode)
@@ -337,7 +364,7 @@ Intent Search_draw(void)
 		Draw_screen_ready(1, back_color);
 		
 		// (!) : I don't know how to draw textures truncated, so I will just fill the margin with white again
-		draw_search_result(search_result_bak, key, color);
+		draw_search_result(search_result_bak, wrapped_titles_bak, key, color);
 		Draw_texture(var_square_image[0], back_color, 0, 0, 320, RESULT_Y_LOW - 1);
 		Draw_texture(var_square_image[0], color, 0, RESULT_Y_LOW - 1, 320, 1);
 		Draw_texture(var_square_image[0], back_color, 0, RESULT_Y_HIGH + 1, 320, 240 - RESULT_Y_HIGH - 1);
@@ -358,6 +385,7 @@ Intent Search_draw(void)
 	}
 	else
 		gspWaitForVBlank();
+	
 
 	if (Util_err_query_error_show_flag()) {
 		Util_err_main(key);
