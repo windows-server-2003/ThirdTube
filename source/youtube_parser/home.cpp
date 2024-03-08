a64ce7e24dd8347d47dfa83be6c77d9bd86654db
windows-server-2003 <rin20042004@gmail.com>
2024-03-08 17:55:00 +0900

    Replace Home with Trending

--- source/youtube_parser/home.cpp
+++ source/youtube_parser/home.cpp
@@ -5,7 +5,7 @@
 YouTubeHomeResult youtube_load_home_page() {
 	YouTubeHomeResult res;
 	
-	std::string post_content = R"({"context": {"client": {"hl": "$0", "gl": "$1", "clientName": "MWEB", "clientVersion": "2.20220407.00.00"}}, "browseId": "FEwhat_to_watch"})";
+	std::string post_content = R"({"context": {"client": {"hl": "$0", "gl": "$1", "clientName": "MWEB", "clientVersion": "2.20220407.00.00"}}, "browseId": "FEtrending"})";
 	post_content = std::regex_replace(post_content, std::regex("\\$0"), language_code);
 	post_content = std::regex_replace(post_content, std::regex("\\$1"), country_code);
 	
@@ -14,10 +14,12 @@ YouTubeHomeResult youtube_load_home_page() {
 		[&] (Document &, RJson yt_result) {
 			res.visitor_data = yt_result["responseContext"]["visitorData"].string_value();
 			for (auto tab : yt_result["contents"]["singleColumnBrowseResultsRenderer"]["tabs"].array_items()) {
-				for (auto item : tab["tabRenderer"]["content"]["richGridRenderer"]["contents"].array_items()) {
-					if (item.has_key("richItemRenderer")) res.videos.push_back(parse_succinct_video(item["richItemRenderer"]["content"]["videoWithContextRenderer"]));
-					if (item.has_key("continuationItemRenderer"))
-						res.continue_token = item["continuationItemRenderer"]["continuationEndpoint"]["continuationCommand"]["token"].string_value();
+				for (auto section : tab["tabRenderer"]["content"]["sectionListRenderer"]["contents"].array_items()) {
+					if (section.has_key("continuationItemRenderer")) 
+						res.continue_token = section["continuationItemRenderer"]["continuationEndpoint"]["continuationCommand"]["token"].string_value();
+					for (auto item : section["itemSectionRenderer"]["contents"].array_items()) {
+						if (item.has_key("videoWithContextRenderer")) res.videos.push_back(parse_succinct_video(item["videoWithContextRenderer"]));
+					}
 				}
 			}
 		},
@@ -50,10 +52,12 @@ void YouTubeHomeResult::load_more_results() {
 		[&] (Document &, RJson yt_result) {
 			if (yt_result["responseContext"]["visitorData"].string_value() != "") visitor_data = yt_result["responseContext"]["visitorData"].string_value();
 			for (auto action : yt_result["onResponseReceivedActions"].array_items()) {
-				for (auto item : action["appendContinuationItemsAction"]["continuationItems"].array_items()) {
-					if (item.has_key("richItemRenderer")) videos.push_back(parse_succinct_video(item["richItemRenderer"]["content"]["videoWithContextRenderer"]));
-					if (item.has_key("continuationItemRenderer"))
-						continue_token = item["continuationItemRenderer"]["continuationEndpoint"]["continuationCommand"]["token"].string_value();
+				for (auto section : action["appendContinuationItemsAction"]["continuationItems"].array_items()) {
+					if (section.has_key("continuationItemRenderer")) 
+						continue_token = section["continuationItemRenderer"]["continuationEndpoint"]["continuationCommand"]["token"].string_value();
+					for (auto item : section["itemSectionRenderer"]["contents"].array_items()) {
+						if (item.has_key("videoWithContextRenderer")) videos.push_back(parse_succinct_video(item["videoWithContextRenderer"]));
+					}
 				}
 			}
 		},
